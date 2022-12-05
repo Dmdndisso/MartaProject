b8d085d15d7ab51fb7972efcefde2ec69191d7e0
Dmytro Sysoiev <dmnikua@gmail.com>
2022-12-05 12:46:39 +0100

    first edition

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.header-slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 3000,
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+});
+
